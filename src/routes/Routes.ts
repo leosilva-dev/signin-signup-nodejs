26bcaea4dd7d4a7b7b1ff69f2f83305da46768bd
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-04-23 18:44:27 -0300

    try: production tests

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -3,6 +3,7 @@ import { SignUpController, SignInController } from '../controllers'
 
 const routes = Router()
 
+routes.get('/', (_, res) => res.send('Working...'));
 routes.post('/sign-up', SignUpController.validateSignUp, SignUpController.signUp)
 routes.post('/sign-in', SignInController.validateSignIn, SignInController.signIn)
 
